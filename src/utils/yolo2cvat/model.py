7db42ce701fd483b6af65a1f2b834dcf4369e42e
CarlosLavilla <carlos.lavilla003@gmail.com>
2026-02-16 16:52:32 +0100

    Add XML generation for CVAT annotations and update YOLO parsing logic

--- src/utils/yolo2cvat/model.py
+++ src/utils/yolo2cvat/model.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from dataclasses import dataclass
+import xml.etree.ElementTree as ET
 
 
 class Class(Enum):
@@ -18,6 +19,30 @@ class CVATBox:
     occluded: int = 0
     source: str = "auto"
 
+    def cvat_box_to_xml(self, element: ET.Element) -> ET.Element:
+        """
+        Transforms the CVAT box representation into XML format.
+
+        :param self: the CVAT box instance
+        :param element: parent element in the XML hierarchy.
+        :type element: ET.Element
+        :return: XML subelement representing a CVAT box
+        :rtype: Element[str]
+        """
+        return ET.SubElement(
+            element,
+            "box",
+            {
+                "label": self.label,
+                "xtl": str(self.xtl),
+                "ytl": str(self.ytl),
+                "xbr": str(self.xbr),
+                "ybr": str(self.ybr),
+                "occluded": str(self.occluded),
+                "source": self.source,
+            },
+        )
+
 
 @dataclass
 class CVATImage:
@@ -26,9 +51,32 @@ class CVATImage:
     height: int
     boxes: list[CVATBox]
 
+    def cvat_img_to_xml(self) -> ET.Element:
+        """
+        Transforms a CVAT Image (frame) into its XML representation.
+
+        :param self: CVAT image instance.
+        :return: XML representation of the image.
+        :rtype: Element[str]
+        """
+        image_element = ET.Element(
+            "image",
+            {
+                "id": str(self.frame_id),
+                "name": f"frame_{self.frame_id:06d}.jpg",
+                "width": str(self.width),
+                "height": str(self.height),
+            },
+        )
+
+        for box in self.boxes:
+            box.cvat_box_to_xml(image_element)
+
+        return image_element
+
 
 @dataclass
-class YoLoAnnotationRow:
+class YoloAnnotationRow:
     annotation_class: Class
     x_center: float
     y_center: float
@@ -37,7 +85,17 @@ class YoLoAnnotationRow:
     confidence: float
 
     def yolo_to_cvat_box(self, img_width: int, img_height: int) -> CVATBox:
+        """
+        Converts a YoLo .txt annotation file into a representation of what CVAT expects for importing.
 
+        :param self: YoloAnnotationRow instance
+        :param img_width: width of the image
+        :type img_width: int
+        :param img_height: height of the image
+        :type img_height: int
+        :return: CVAT representation of the YoLo .txt file
+        :rtype: CVATBox
+        """
         # YoLo normalizes values, I de-normalize them
         x_center = self.x_center * img_width
         y_center = self.y_center * img_height
--- src/utils/yolo2cvat/yolo_2_cvat.py
+++ src/utils/yolo2cvat/yolo_2_cvat.py
@@ -1,14 +1,20 @@
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
-from utils.yolo2cvat.model import YoLoAnnotationRow, Class
+from utils.yolo2cvat.model import YoloAnnotationRow, Class, CVATBox, CVATImage
 
 PREDICTIONS_DIR = (
     Path(__file__).resolve().parents[3] / "runs" / "detect" / "predictions"
 )
-IMG_WIDTH = 0
-IMG_HEIGHT = 0
+IMG_WIDTH = 1920
+IMG_HEIGHT = 1080
 
-CONFIDENCE_THRESHOLD = 0
+# Different thresholds for each category due to different difficulties on detection
+CONFIDENCE_THRESHOLD: dict[Class, float] = {
+    Class.PLAYER: 0.5,
+    Class.REFEREE: 0.4,
+    Class.BALL: 0.3,
+}
 
 
 def extract_frame_id(stem: str) -> int:
@@ -16,10 +22,10 @@ def extract_frame_id(stem: str) -> int:
     return int(stem.split("_")[-1])
 
 
-def parse_yolo_line(line: str) -> YoLoAnnotationRow:
+def parse_yolo_line(line: str) -> YoloAnnotationRow:
     parts = line.strip().split()
 
-    return YoLoAnnotationRow(
+    return YoloAnnotationRow(
         annotation_class=Class(int(parts[0])),
         x_center=float(parts[1]),
         y_center=float(parts[2]),
@@ -29,20 +35,51 @@ def parse_yolo_line(line: str) -> YoLoAnnotationRow:
     )
 
 
+def generate_xml(frames: dict[int, CVATImage]) -> ET.ElementTree:
+    # Create root XML element
+    root = ET.Element("annotations")
+
+    # Create header (metadata)
+    meta = ET.SubElement(root, "meta")
+    task = ET.SubElement(meta, "task")
+    labels = ET.SubElement(task, "labels")
+
+    # Place the classes
+    for cls in Class:
+        label = ET.SubElement(labels, "label")
+        name = ET.SubElement(label, "name")
+        name.text = cls.name.lower()
+
+    # Place the annotations
+    for frame_id in sorted(frames.keys()):
+        image_element = frames[frame_id].cvat_img_to_xml()
+        root.append(image_element)
+
+    return ET.ElementTree(root)
+
+
 if not PREDICTIONS_DIR.exists():
     raise FileNotFoundError(f"Incorrect directory: {PREDICTIONS_DIR}")
+
+frames: dict[int, CVATImage] = {}
 
 for txt_file in PREDICTIONS_DIR.glob("**/*.txt"):
     frame_id = extract_frame_id(txt_file.stem)
 
     with open(txt_file, "r") as f:
         for line in f:
-            print(line)
-            row = parse_yolo_line(line)
+            row: YoloAnnotationRow = parse_yolo_line(line)
 
-            if row.confidence < CONFIDENCE_THRESHOLD:
+            if row.confidence < CONFIDENCE_THRESHOLD[row.annotation_class]:
                 continue
 
-            box = row.yolo_to_cvat_box(IMG_WIDTH, IMG_HEIGHT)
+            box: CVATBox = row.yolo_to_cvat_box(IMG_WIDTH, IMG_HEIGHT)
 
             # store box grouped by frame_id
+            frames.setdefault(
+                frame_id, CVATImage(frame_id, IMG_WIDTH, IMG_HEIGHT, [])
+            ).boxes.append(box)
+
+xml_file: ET.ElementTree = generate_xml(frames)
+
+xml_file.write("annotations.xml", encoding="utf-8", xml_declaration=True)
